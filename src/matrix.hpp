7f4c7ad6090281812cb7a365af4797114fb73556
crissyro <morozdef24@gmail.com>
2024-10-02 23:47:12 +0300

    remake args in func

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -11,20 +11,20 @@ private:
     size_t rows_, cols_;
     T **data_;
 
-    void initMatrix(void);
-    void freeMemoryMatrix(void);
+    void initMatrix();
+    void freeMemoryMatrix();
 
 public:
-    size_t getRows(void) const { return rows_; };
-    size_t getCols(void) const { return cols_; };
+    size_t getRows() const { return rows_; };
+    size_t getCols() const { return cols_; };
     void makeMatrixFromArray(const T** array);
-    Matrix(void);
+    Matrix();
     Matrix(const size_t size);
     Matrix(const size_t rows, const size_t cols);
     Matrix(const size_t rows, const size_t cols, T** array);
     Matrix(const Matrix& other);
     Matrix(Matrix&& other);
-    ~Matrix(void);
+    ~Matrix();
 
     Matrix& operator=(const Matrix& other);
     bool operator==(const Matrix& other);
@@ -46,23 +46,23 @@ public:
     bool isEMatrix(const Matrix& other);
     bool isIdentityMatrix(const Matrix& other);
     bool isZeroMatrix(const Matrix& other);
-    bool isSingular(void);
+    bool isSingular();
 
 
     void sumMatrix(const Matrix& other);
     voud subMatrix(const Matrix& other);
     void mulMatrix(const Matrix& other);
     void mulScalar(const T scalar);
-    void transposeMatrix(void);
+    void transposeMatrix();
     Matrix transposeMatrix(const Matrix& other);
-    void transposeSquareMatrix(void);
+    void transposeSquareMatrix();
     Matrix transposeSquareMatrix(const Matrix& other);
 
-    T determinant(void);
-    Matrix cofactorMatrix(void);
-    Matrix adjugateMatrix(void);
-    Matrix inverseMatrix(void);
+    T determinant();
+    Matrix cofactorMatrix();
+    Matrix adjugateMatrix();
+    Matrix inverseMatrix();
     Matrix inverseMatrix(const Matrix& other);
-    Matrix calcComplementsMatrix(void);
+    Matrix calcComplementsMatrix();
 
 };
