d9f1de79b5106f0820937beb94575bc99b30ffb5
crissyro <morozdef24@gmail.com>
2024-10-08 20:43:51 +0300

    constructors and destructors

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -23,6 +23,9 @@ void Matrix<T>::freeMemoryMatrix() {
         delete[] data_;
         data_ = nullptr;
     }
+
+    rows_ = 0;
+    cols_ = 0;
 }
 
 template<typename T>
@@ -32,3 +35,36 @@ void Matrix<T>::copyMatrix(const Matrix<T>& other) {
     for (size_t i = 0; i < rows_; i++)
         std::copy(other.data_[i], other.data_[i] + cols_, data[i]);
 }
+
+template<typename T>
+Matrix<T>::Matrix(const size_t rows, const size_t cols): rows_(rows), cols_(cols), data_{nullptr} {
+    initMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(): rows_(MIN_SIZE_MATRIX), cols_(MIN_SIZE_MATRIX), data_{nullptr}{
+    initMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(const size_t size) {
+    Matrix<T>::Matrix(size, size);
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix<T>& other): rows_(other.rows_), cols_(other.cols_), data_(nullptr) {
+    initMatrix();
+    copyMatrix(other);
+}
+
+template<typename T>
+Matrix<T>::Matrix(Matrix&& other): rows_(other.rows_), cols_(other.cols_), data_(other.data_) {
+    other.freeMemoryMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(const size_t rows, const size_t cols, T** array): rows_(rows), cols_(cols), data_(array) { }
+
+template<typename T>
+Matrix<T>::~Matrix() { freeMemoryMatrix(); }
+
