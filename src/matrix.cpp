d22fe855a76b89b45a0b1ad74a22e1d2ab99bf7d
crissyro <morozdef24@gmail.com>
2024-10-12 16:53:04 +0300

    make constructors

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -25,5 +25,37 @@ void Matrix<T>::copyMatrix(const Matrix<T>& other) {
 
     for (size_t i = 0; i < rows_; ++i) 
         std::copy(other.data_[i].get(), other.data_[i].get() + cols_, data_[i].get());
-    
+}
+
+template<typename T>
+Matrix<T>::Matrix() noexcept : rows_(MIN_SIZE_MATRIX), cols_(MIN_SIZE_MATRIX) {
+    initMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(const size_t size) noexcept : rows_(size), cols_(size) {
+    initMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(const size_t rows, const size_t cols) noexcept : rows_(rows), cols_(cols) {
+    initMatrix();
+}
+
+template<typename T>
+Matrix<T>::Matrix(const size_t rows, const size_t cols, T** array) noexcept : rows_(rows), cols_(cols) {
+    initMatrix();
+    for (size_t i = 0; i < rows_; ++i) 
+        std::copy(array[i], array[i] + cols_, data_[i].get());
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix<T>& other) noexcept {
+    copyMatrix(other);
+}
+
+template<typename T>
+Matrix<T>::Matrix(Matrix<T>&& other) noexcept : rows_(other.rows_), cols_(other.cols_), data_(std::move(other.data_)) {
+    other.rows_ = 0;
+    other.cols_ = 0;
 }
